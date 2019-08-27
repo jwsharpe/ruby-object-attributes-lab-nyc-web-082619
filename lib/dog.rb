fe8ba95edcf972ffa821ed4a521fcb7a510c60d0
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-27 13:54:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,15 +4,15 @@ class Dog
     @name
   end
   
-  def name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end
   
   def breed
     @breed
   end
   
-  def breed=(dog_breed)
-    @breed = dog_breed
+  def breed=(breed)
+    @breed = breed
   end
 end
